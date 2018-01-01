6b0ca8e676470fe5f7ad6fa9ac3936a4bc1ddaee
Michael Dyrynda <michael@dyrynda.com.au>
2018-01-01 21:26:07 +1030

    add navigation collection, selected page helper

--- config.php
+++ config.php
@@ -4,4 +4,12 @@ return [
     'baseUrl' => '',
     'production' => false,
     'collections' => [],
+    'selected' => function ($page, $path) {
+        $pages = collect(array_wrap($page));
+
+        return $pages->contains(function ($page) use ($path) {
+            return str_contains($page->getPath(), $path);
+        });
+    },
+    'navigation' => require_once('navigation.php'),
 ];
---
+++ source/_layouts/navigation.blade.php
@@ -0,0 +1,7 @@
+<ul class="px-8 mt-6 text-base">
+    @foreach ($page->navigation as $title => $link)
+        <li class="leading-loose {{ $page->selected($link) ? 'font-medium' : 'font-light' }}">
+            <a href="{{ $link }}">{{ $title }}</a>
+        </li>
+    @endforeach
+</ul>
