80c7ccaf0ec5849f67628a66398384e91615b0a7
Michael Dyrynda <michael@dyrynda.com.au>
2018-01-02 13:11:01 +1030

    Add leading slash to navigation links

--- navigation.php
+++ navigation.php
@@ -1,15 +1,15 @@
 <?php
 
 return [
-    'General Style' => 'general-style',
-    'Docblocks &amp; Comments' => 'docblocks-comments',
-    'Conditionals' => 'conditionals',
-    'Configuration' => 'configuration',
-    'Routing' => 'routing',
-    'Controllers' => 'controllers',
-    'Views' => 'views',
-    'Validation' => 'validation',
-    'Requests' => 'requests',
-    'Eloquent' => 'eloquent',
-    'Migrations &amp; Schema' => 'migrations-schema',
+    'General Style' => '/general-style',
+    'Docblocks &amp; Comments' => '/docblocks-comments',
+    'Conditionals' => '/conditionals',
+    'Configuration' => '/configuration',
+    'Routing' => '/routing',
+    'Controllers' => '/controllers',
+    'Views' => '/views',
+    'Validation' => '/validation',
+    'Requests' => '/requests',
+    'Eloquent' => '/eloquent',
+    'Migrations &amp; Schema' => '/migrations-schema',
 ];
