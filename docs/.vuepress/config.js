748e87a781e388ac65df41994ee0eb5d1c1460de
Michael Dyrynda <michael@dyrynda.com.au>
2019-04-07 20:26:18 +0930

    add nav links

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,6 +2,10 @@ module.exports = {
     title: "Founder Style Guide",
     description: "A set of styles and conventions to follow when building Laravel projects.",
     themeConfig: {
+        nav: [
+            { text: 'Founder', link: 'https://github.com/michaeldyrynda/founder.git' },
+            { text: 'GitHub', link: 'https://github.com/michaeldyrynda/founder-style.git' }
+        ],
         sidebar: [
             ['/', 'Founder'],
             '/production/',
